a6385713407f1de2628c9e5a6618a591af3bbda9
KOPRIYA <66168752+KOPRIYA@users.noreply.github.com>
2021-10-24 16:31:34 +0530

    Create Present_Value_of_an_Annuity.java

---
+++ Present_Value_of_an_Annuity.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.Scanner;
+class PV
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the annuity amount : ");
+	      double annuity_val=sc.nextInt();
+	      System.out.print("Enter the interest rate in percentage: ");
+	      double r = sc.nextInt();
+	      System.out.print("Enter the time period in years: ");
+	      double n = sc.nextInt();
+	      double pv = present_val * ((1-1/Math.pow((1+r/100),n))/(r/100));
+	      System.out.print("Present value of the annuity: " +annuity_val + "at an interest rate of: " +r + "is: " +pv);
+  }
+}
